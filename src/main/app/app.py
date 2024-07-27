c4eeb5fc9ac151f667fe81dea895e6e20245ffb7
Alex Omarov <shtil.a@yandex.ru>
2024-07-28 00:24:26 +0700

    ML-6 Added gpu support

--- src/main/app/app.py
+++ src/main/app/app.py
@@ -5,6 +5,7 @@ import os
 
 import scipy
 import torch
+
 from langchain_community.chat_models import ChatOllama
 from langchain_core.messages import HumanMessage, BaseMessage
 from transformers import AutoProcessor, MusicgenForConditionalGeneration
@@ -14,21 +15,19 @@ from business.util.ml_logger.logger import create_logger
 
 def _generate(text: str):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
-    model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
+    # processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
+    # model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
     print(device)
-    inputs = processor(
-        text=[
-
-        ],
-        padding=True,
-        return_tensors="pt",
-    ).to(device)
-
-    audio_values = model.generate(**inputs, max_new_tokens=512)
-    print("Generated")
-    sampling_rate = model.config.audio_encoder.sampling_rate
-    scipy.io.wavfile.write("musicgen_out.wav", rate=sampling_rate, data=audio_values[0, 0].numpy())
+    # inputs = processor(
+    #     text=[text],
+    #     padding=True,
+    #     return_tensors="pt",
+    # ).to(device)
+#
+    # audio_values = model.generate(**inputs, max_new_tokens=512)
+    # print("Generated")
+    # sampling_rate = model.config.audio_encoder.sampling_rate
+    # scipy.io.wavfile.write("musicgen_out.wav", rate=sampling_rate, data=audio_values[0, 0].numpy())
 
 
 def _call_chat(content: str) -> BaseMessage:
@@ -46,5 +45,4 @@ def _call_chat(content: str) -> BaseMessage:
     return res
 
 
-_generate("Deep, dark, scary echo ambient sound of a cave with water drops, filled with distant voices singing dark "
-          "choir like underground demons")
+_generate("American pop-rock")
