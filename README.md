cce27b9c5ba2df008d95d141133008728e8e1322
Alex Omarov <shtil.a@yandex.ru>
2024-07-27 13:36:05 +0700

    ML-4 Added huggingface transformers, added musicgenre

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Service for ai assistant for tRPG.
 
-[![Python version](https://img.shields.io/static/v1?label=Python&message=3.11&color=blue)](https://sonarcloud.io/summary/new_code?id=AlexOmarov_berte-ai-service)
+[![Python version](https://img.shields.io/static/v1?label=Python&message=3.12&color=blue)](https://sonarcloud.io/summary/new_code?id=AlexOmarov_berte-ai-service)
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=AlexOmarov_berte-ai-service&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=AlexOmarov_berte-ai-service)
-[![Maintainability Rating](https://sonarcloud.io/api/project_badges/measure?project=AlexOmarov_berte-ai-service&metric=sqale_rating)](https://sonarcloud.io/summary/new_code?id=AlexOmarov_berte-ai-service)
-[![Security Rating](https://sonarcloud.io/api/project_badges/measure?project=AlexOmarov_berte-ai-service&metric=security_rating)](https://sonarcloud.io/summary/new_code?id=AlexOmarov_berte-ai-service)
 [![Coverage](https://sonarcloud.io/api/project_badges/measure?project=AlexOmarov_berte-ai-service&metric=coverage)](https://sonarcloud.io/summary/new_code?id=AlexOmarov_berte-ai-service)
--- src/main/app/app.py
+++ src/main/app/app.py
@@ -3,10 +3,33 @@ Script for executing first call to chatLLM
 """
 import os
 
+import scipy
+import torch
 from langchain_community.chat_models import ChatOllama
 from langchain_core.messages import HumanMessage, BaseMessage
+from transformers import AutoProcessor, MusicgenForConditionalGeneration
 
 from business.util.ml_logger.logger import create_logger
+
+
+def _generate():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
+    model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
+    print(device)
+    inputs = processor(
+        text=[
+            "Deep, dark, scary echo ambient sound of a cave with water drops, filled with distant voices singing dark "
+            "choir like underground demons"
+            ],
+        padding=True,
+        return_tensors="pt",
+    ).to(device)
+
+    audio_values = model.generate(**inputs, max_new_tokens=512)
+    print("Generated")
+    sampling_rate = model.config.audio_encoder.sampling_rate
+    scipy.io.wavfile.write("musicgen_out.wav", rate=sampling_rate, data=audio_values[0, 0].numpy())
 
 
 def _call_chat(content: str) -> BaseMessage:
@@ -22,3 +45,6 @@ def _call_chat(content: str) -> BaseMessage:
     res = llm.invoke(messages)
     logger.info(res)
     return res
+
+
+_generate()
