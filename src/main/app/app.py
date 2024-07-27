9515932849c20b128eb9d09a9cc2c005e76d093c
Alex Omarov <shtil.a@yandex.ru>
2024-07-28 00:41:58 +0700

    ML-7 Music experiment

--- src/main/app/app.py
+++ src/main/app/app.py
@@ -15,19 +15,17 @@ from business.util.ml_logger.logger import create_logger
 
 def _generate(text: str):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
-    # model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
-    print(device)
-    # inputs = processor(
-    #     text=[text],
-    #     padding=True,
-    #     return_tensors="pt",
-    # ).to(device)
-#
-    # audio_values = model.generate(**inputs, max_new_tokens=512)
-    # print("Generated")
-    # sampling_rate = model.config.audio_encoder.sampling_rate
-    # scipy.io.wavfile.write("musicgen_out.wav", rate=sampling_rate, data=audio_values[0, 0].numpy())
+    processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
+    model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
+    inputs = processor(
+        text=[text],
+        padding=True,
+        return_tensors="pt",
+    ).to(device)
+
+    audio_values = model.generate(**inputs, max_new_tokens=512)
+    sampling_rate = model.config.audio_encoder.sampling_rate
+    scipy.io.wavfile.write("musicgen_out.wav", rate=sampling_rate, data=audio_values[0, 0].numpy())
 
 
 def _call_chat(content: str) -> BaseMessage:
