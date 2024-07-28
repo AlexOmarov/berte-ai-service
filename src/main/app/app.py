87d3ac35bf3a60555511cab4266a0c3875f89159
Alex Omarov <shtil.a@yandex.ru>
2024-07-28 14:23:21 +0700

    ML-7 Fix gpu bug

--- src/main/app/app.py
+++ src/main/app/app.py
@@ -16,16 +16,16 @@ from business.util.ml_logger.logger import create_logger
 def _generate(text: str):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
-    model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
+    model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small").to(device)
     inputs = processor(
         text=[text],
         padding=True,
         return_tensors="pt",
     ).to(device)
 
-    audio_values = model.generate(**inputs, max_new_tokens=512)
+    audio_values = model.generate(**inputs, max_new_tokens=2048)
     sampling_rate = model.config.audio_encoder.sampling_rate
-    scipy.io.wavfile.write("musicgen_out.wav", rate=sampling_rate, data=audio_values[0, 0].numpy())
+    scipy.io.wavfile.write("musicgen_out.wav", rate=sampling_rate, data=audio_values[0, 0].cpu().numpy())
 
 
 def _call_chat(content: str) -> BaseMessage:
@@ -43,4 +43,4 @@ def _call_chat(content: str) -> BaseMessage:
     return res
 
 
-_generate("American pop-rock")
+_generate("Russian traditional music")
